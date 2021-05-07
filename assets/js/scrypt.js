725ae2bc16f6bbd39c585952e4a1c36bc4895b0a
Nikola <88djony@gmail.com>
2021-05-07 14:04:09 -0700

    API link, ajax function and search function

--- assets/js/scrypt.js
+++ assets/js/scrypt.js
@@ -11,3 +11,37 @@ var currentHumidity = $("#humidity");
 var currentUvindex = $("#uvIndex");
 var srchCity=[];
 
+// open weather API key
+var APIkey = "f2e60cb0b34680eabdf19a9dec1b7967";
+
+function displayWeather(event){
+    event.preventDefault();
+    if(searchCity.val().trim()!==""){
+        city=searchCity.val().trim();
+        currentWeather(city);
+    }
+}
+
+// Ajax function
+function currentWeather(city){
+    // API url
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIkey;
+    $.ajax({
+        url:queryURL,
+        method:"GET",
+    }).then(function(response){
+
+        console.log(response);
+    })
+
+}
+// Checks input if it exist in local storage history
+function find(c){
+    for (var i=0; i<srchCity.length; i++){
+        if(c.toUpperCase()===srchCity[i]){
+            return -1;
+        }
+    }
+
+    return -1;
+}
