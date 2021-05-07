4f9b9ff83e1e8f868e4008febce18e84671c2667
Nikola <88djony@gmail.com>
2021-05-07 13:51:28 -0700

    add variables to js

---
+++ assets/js/scrypt.js
@@ -0,0 +1,13 @@
+// variable declaration
+
+var city="";
+var searchCity = $("#searchCity");
+var searcButton = $("#serachButton");
+var currentCity = $("#currentCity");
+var clearButton = $("#clearHistory");
+var currentTemp = $("#temperature");
+var currentSpeed = $("#windSpeed");
+var currentHumidity = $("#humidity");
+var currentUvindex = $("#uvIndex");
+var srchCity=[];
+
